30dbd801ebb2d0cbe4ffb9d173f12cbbe1b2cb28
pereskokovaoi <pereskokovaoi@gmail.com>
2022-11-24 16:53:01 +0500

    added function get_name()

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 def hello():
     print("Hello, World!")
 
+
+def get_name():
+    print("Как тебя зовут?")
+    name = input()
+    return name
+
+
 if __name__ == '__main__':
     hello()
+    name = get_name()
+    print(f"Добро пожаловать, {name}!")
